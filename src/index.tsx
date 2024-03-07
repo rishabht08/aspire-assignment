75de4a2aea9002d0bc054e8268b5a5aa3c321ad7
rishabt08 <rishabht123@gmail.com>
2024-03-07 18:30:58 +0530

    url changes

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={Store}>
-      <BrowserRouter>
+      <BrowserRouter basename='/aspire-assignment'>
         <App />
       </BrowserRouter>
     </Provider>
